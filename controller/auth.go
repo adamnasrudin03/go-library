9c9416a303028df3c44effa56252aac15509beb8
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-10 14:38:09 +0700

    setup auth controller

---
+++ controller/auth.go
@@ -0,0 +1,35 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//AuthController interface is a contract what this controller can do
+type AuthController interface {
+	Login(ctx *gin.Context)
+	Register(ctx *gin.Context)
+}
+
+type authController struct {
+	//this is where you put your service
+}
+
+//NewAuthController creates a new instance of AuthController
+func NewAuthController() AuthController {
+	return &authController{}
+}
+
+func (c *authController) Login(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"messages" : "Hello word",
+	})
+}
+
+func (c *authController) Register(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"messages" : "Hello word",
+	})
+	
+}
