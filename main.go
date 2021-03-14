fbc722f4a0781a687a76dc3373d8e39fdb68dfd4
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-14 08:46:49 +0700

    config controller

--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"go-library/config"
 	"go-library/controller"
+	"go-library/repository"
+	"go-library/service"
 
 	"github.com/gin-gonic/gin"
+
 	"gorm.io/gorm"
 )
 
 
 var (
 	db             *gorm.DB                  = config.SetupDatabaseConnection()
-	authController controller.AuthController = controller.NewAuthController()
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	jwtService     service.JWTService        = service.NewJWTService()
+	authService    service.AuthService       = service.NewAuthService(userRepository)
+	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
 )
 func main() {
 	defer config.CloseDatabaseConnection(db)
